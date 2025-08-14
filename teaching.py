19b646d4a3ca885be7ef07d89fe7162dec4d4875
Limaem1 <80838682+Limaem1@users.noreply.github.com>
2025-08-14 21:30:11 +0200

    created functions

--- teaching.py
+++ teaching.py
@@ -1,1 +1,18 @@
-name 
+number1 = 1
+number2 = 2 
+x = 0 
+
+def add(number1,number2):
+    return number1 + number2
+
+
+def multiply(number1,number2):
+    x = 0
+    for i in range(number1):
+        x = add(x,number2)
+    return x
+
+
+result1 = add(number1,number2)
+result2 = multiply(number1,number2)
+print(result1,result2)
